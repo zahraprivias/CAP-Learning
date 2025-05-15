7718b32ff1f33ca2cb3e4f8f80f9846032fd2501
zahraprivias <zahraprivias@gmail.com>
2025-05-15 13:14:08 +0700

    Add first CAP project

---
+++ zcap_sqllite/srv/src/main/java/com/zahra/cap/zcap_sqllite/Application.java
@@ -0,0 +1,16 @@
+/************************************************************************
+ * © 2019-2025 SAP SE or an SAP affiliate company. All rights reserved. *
+ ************************************************************************/
+package com.zahra.cap.zcap_sqllite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
